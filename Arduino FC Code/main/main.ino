ab24a78f77c28fd8ac87f79fa820a25716fd25ee
Benno <81779508+Westpol@users.noreply.github.com>
2025-03-03 16:21:47 +0100

    Create main.ino

---
+++ Arduino FC Code/main/main.ino
@@ -0,0 +1,13 @@
+
+// Search for: "FlashStorage_STM32" in the library manager
+#include "FlashStorage_STM32.h"
+
+void setup() {
+  Serial.begin(115200);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
