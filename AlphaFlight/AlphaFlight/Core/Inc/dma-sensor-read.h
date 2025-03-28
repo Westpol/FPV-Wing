44bd4029e29899c6d2de335df237b65e914733d1
Benno <81779508+Westpol@users.noreply.github.com>
2025-03-28 13:13:42 +0100

    added dma-sensor-read library

---
+++ AlphaFlight/AlphaFlight/Core/Inc/dma-sensor-read.h
@@ -0,0 +1,13 @@
+/*
+ * dma-sensor-read.h
+ *
+ *  Created on: Mar 28, 2025
+ *      Author: benno
+ */
+
+#ifndef INC_DMA_SENSOR_READ_H_
+#define INC_DMA_SENSOR_READ_H_
+
+
+
+#endif /* INC_DMA_SENSOR_READ_H_ */
---
+++ AlphaFlight/AlphaFlight/Core/Src/sma-sensor-read.c
@@ -0,0 +1,9 @@
+/*
+ * sma-sensor-read.c
+ *
+ *  Created on: Mar 28, 2025
+ *      Author: benno
+ */
+
+#include "dma-sensor-read.h"
+
