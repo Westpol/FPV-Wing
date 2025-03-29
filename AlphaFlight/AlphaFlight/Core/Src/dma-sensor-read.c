4655a9de22a999b837f499c83893a48e620fe54b
Benno <81779508+Westpol@users.noreply.github.com>
2025-03-29 15:54:58 +0100

    renamed dma-sensor-read.c file correctly

--- AlphaFlight/AlphaFlight/Core/Src/dma-sensor-read.c
+++ AlphaFlight/AlphaFlight/Core/Src/dma-sensor-read.c
@@ -53,6 +53,7 @@ void DMA_READ_SPI_SENSORS(SPI_HandleTypeDef *HSPIx, uint8_t *gyro_rx, uint8_t *a
 	uint8_t tx_buffer[7] = {GYRO_RATE_DATA_ADDRESS, 0, 0, 0, 0, 0, 0};
 	sensor_point = 1;
 	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_0, GPIO_PIN_RESET);
+	GYRO_NEW = true;
 	HAL_SPI_TransmitReceive_DMA(hspi, tx_buffer, recieve_data_pointers.gyro, 7);
 }
 
@@ -64,6 +65,7 @@ void HAL_SPI_RxTxCpltCallback(SPI_HandleTypeDef *hspi){
 			uint8_t tx_buffer_accel[8] = {ACCEL_ACCELERATION_DATA_ADDRESS, 0, 0, 0, 0, 0, 0, 0};
 			HAL_GPIO_WritePin(GPIOB, GPIO_PIN_0, GPIO_PIN_SET);
 			HAL_GPIO_WritePin(GPIOB, GPIO_PIN_1, GPIO_PIN_RESET);
+			ACCEL_NEW = true;
 			HAL_SPI_TransmitReceive_DMA(hspi, tx_buffer_accel, recieve_data_pointers.accel, 8);
 			break;
 		case 2:
@@ -71,6 +73,7 @@ void HAL_SPI_RxTxCpltCallback(SPI_HandleTypeDef *hspi){
 			uint8_t tx_buffer_baro[8] = {0x04 | READ_BYTE, 0, 0, 0, 0, 0, 0, 0};
 			HAL_GPIO_WritePin(GPIOB, GPIO_PIN_1, GPIO_PIN_SET);
 			HAL_GPIO_WritePin(GPIOC, GPIO_PIN_4, GPIO_PIN_RESET);
+			BARO_NEW = true;
 			HAL_SPI_TransmitReceive_DMA(hspi, tx_buffer_baro, recieve_data_pointers.baro, 8);
 			break;
 		default:
