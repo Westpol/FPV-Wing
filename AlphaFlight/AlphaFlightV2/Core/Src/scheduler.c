28913ff6f218e8b960a83185d4bbe7139a767cb1
Benno <81779508+Westpol@users.noreply.github.com>
2025-04-24 23:16:58 +0200

    Made scheduler overflow-safe

--- AlphaFlight/AlphaFlightV2/Core/Src/scheduler.c
+++ AlphaFlight/AlphaFlightV2/Core/Src/scheduler.c
@@ -36,7 +36,7 @@ void SCHEDULER_ADD_TASK(task_func_t task_func, uint32_t period){	// add tasks in
 void SCHEDULER_UPDATE(){
 	current_time = MICROS();
 	for(int i = 0; i < task_count; i++){
-		if(tasks[i].time_to_execute <= current_time){
+		if((int32_t)(current_time - tasks[i].time_to_execute) >= 0){
 			tasks[i].time_last_execute = current_time;
 			tasks[i].time_to_execute += tasks[i].period;
 			tasks[i].task_func();
