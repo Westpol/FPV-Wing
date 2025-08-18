39a0842432f2d17fb40c1593905e84fa58e685be
Benno <81779508+Westpol@users.noreply.github.com>
2025-08-18 14:42:56 +0200

    Updated pid loop and worked on data representation

--- AlphaFlight Blackbox Viewer/simple_plot.py
+++ AlphaFlight Blackbox Viewer/simple_plot.py
@@ -2,16 +2,24 @@ import matplotlib.pyplot as plt
 import csv
 
 timestamps = []
-values = []
+baro_height = []
+crsf_throttle = []
 
 with open("test.csv") as f:
     reader = csv.reader(f)
     for row in reader:
-        timestamps.append(int(row[0]))
-        values.append(int(row[1]))
+        try:
+            timestamps.append(int(row[0]))
+            baro_height.append(float(row[3]))
+            crsf_throttle.append(int(row[13]) / 40)
+        except:
+            print(row)
 
-plt.plot(values)
-plt.ylabel("Throttle")
-plt.title("Throttle Curve")
+plt.plot(baro_height, label="Baro Height")
+plt.plot(crsf_throttle, label="Throttle input")
+plt.plot()
+plt.ylabel("Stuff")
+plt.title("Fuck yeh")
 plt.grid()
+plt.legend()
 plt.show()
--- AlphaFlight/AlphaFlightV2/Core/Src/attitude_pid.c
+++ AlphaFlight/AlphaFlightV2/Core/Src/attitude_pid.c
@@ -22,6 +22,12 @@ extern IMU_Data imu_data;
 FLY_BY_WIRE_PID_VALUES attitude_pid = {0};
 static FLY_BY_WIRE_PID fbw_pid_settings = {0};
 
+float angle_x_lowpass = 0;
+float angle_y_lowpass = 0;
+
+const float lowpass_alpha_x = 0.2;
+const float lowpass_alpha_y = 0.2;
+
 static void FC_PID_MIXER(float pitchDeflection, float rollDeflection, float throttle){
 	float servoLeft = pitchDeflection + rollDeflection;
 	float servoRight = pitchDeflection - rollDeflection;
@@ -37,14 +43,14 @@ static void FC_PID_MIXER(float pitchDeflection, float rollDeflection, float thro
 }
 
 void FC_PID_INIT(){
-	fbw_pid_settings.pitch_d = 0.01;
-	fbw_pid_settings.pitch_p = 0.01;
+	fbw_pid_settings.pitch_d = 0.1;
+	fbw_pid_settings.pitch_p = 0.1;
 	fbw_pid_settings.pitch_i = 0.001;
-	fbw_pid_settings.pitch_gain = 0.01;
-	fbw_pid_settings.roll_d = 0.01;
-	fbw_pid_settings.roll_p = 0.01;
+	fbw_pid_settings.pitch_gain = 0.1;
+	fbw_pid_settings.roll_d = 0.1;
+	fbw_pid_settings.roll_p = 0.1;
 	fbw_pid_settings.roll_i = 0.001;
-	fbw_pid_settings.roll_gain = 0.01;
+	fbw_pid_settings.roll_gain = 0.1;
 }
 
 void FC_PID_DIRECT_CONTROL(bool armed){
@@ -57,12 +63,16 @@ void FC_PID_DIRECT_CONTROL(bool armed){
 }
 
 void FC_PID_FLY_BY_WIRE_WITHOUT_LIMITS(bool armed, uint32_t dt){
-	attitude_pid.pitch_error = fly_by_wire_setpoints.pitch_angle - imu_data.angle_x_fused;
-	attitude_pid.roll_error = fly_by_wire_setpoints.roll_angle - imu_data.angle_y_fused;
+	float dt_seconds = dt / 1000000.0;
+	if(dt_seconds == 0.0f) return;
+	angle_x_lowpass = imu_data.angle_x_fused * lowpass_alpha_x + angle_x_lowpass * (1 -lowpass_alpha_x);
+	angle_y_lowpass = imu_data.angle_y_fused * lowpass_alpha_y + angle_y_lowpass * (1 -lowpass_alpha_y);
+	attitude_pid.pitch_error = fly_by_wire_setpoints.pitch_angle - angle_x_lowpass;
+	attitude_pid.roll_error = fly_by_wire_setpoints.roll_angle - angle_x_lowpass;
 	//attitude_pid.pitch_error_accumulated = UTIL_MAX_F(UTIL_MIN_F(attitude_pid.pitch_error_accumulated + (attitude_pid.pitch_error * fbw_pid_settings.pitch_i * dt), -0.15), 0.15);
 	//attitude_pid.roll_error_accumulated = UTIL_MAX_F(UTIL_MIN_F(attitude_pid.roll_error_accumulated + (attitude_pid.roll_error * fbw_pid_settings.roll_i * dt), -0.15), 0.15);
-	attitude_pid.pitch_pid_correction = -((attitude_pid.pitch_error * fbw_pid_settings.pitch_p) + attitude_pid.pitch_error_accumulated + (((attitude_pid.pitch_error - attitude_pid.pitch_error_last) / dt) * fbw_pid_settings.pitch_d)) + fbw_pid_settings.pitch_gain;
-	attitude_pid.roll_pid_correction = -((attitude_pid.roll_error * fbw_pid_settings.roll_p) + attitude_pid.roll_error_accumulated + (((attitude_pid.roll_error - attitude_pid.roll_error_last) / dt) * fbw_pid_settings.roll_d)) + fbw_pid_settings.roll_gain;
+	attitude_pid.pitch_pid_correction = -((attitude_pid.pitch_error * fbw_pid_settings.pitch_p) + attitude_pid.pitch_error_accumulated + (((attitude_pid.pitch_error - attitude_pid.pitch_error_last) / dt_seconds) * fbw_pid_settings.pitch_d)) * fbw_pid_settings.pitch_gain;
+	attitude_pid.roll_pid_correction = -((attitude_pid.roll_error * fbw_pid_settings.roll_p) + attitude_pid.roll_error_accumulated + (((attitude_pid.roll_error - attitude_pid.roll_error_last) / dt_seconds) * fbw_pid_settings.roll_d)) * fbw_pid_settings.roll_gain;
 	attitude_pid.pitch_error_last = attitude_pid.pitch_error;
 	attitude_pid.roll_error_last = attitude_pid.roll_error;
 
