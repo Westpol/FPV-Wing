df71c8b9840da60d59c6a60ebe101e3aca4aeaed
Benno <81779508+Westpol@users.noreply.github.com>
2025-08-25 18:38:08 +0200

    Worked on Telemetry to Laptop bridge

--- Telemetry Interface/taranis_esp32_crsf_diy_sensors/taranis_esp32_crsf_diy_sensors.ino
+++ Telemetry Interface/taranis_esp32_crsf_diy_sensors/taranis_esp32_crsf_diy_sensors.ino
@@ -1,4 +1,11 @@
+#include <WiFi.h>
+
+const char *ssid = "ESP32_Telemetry";
+const char *password = "12345678";
+
 HardwareSerial mySerial(2);
+
+WiFiUDP udp;
 
 #define CRSF_SYNC_BYTE 0xC8 // RC packets (TX → RX)
 #define CRSF_PACKET_LENGTH 0x0A
@@ -8,9 +15,20 @@ uint16_t channels[16];
 uint8_t buffer[64];  // Buffer to store incoming data
 uint8_t bufferIndex = 0;
 
+const uint16_t udpPort = 8888;
+IPAddress broadcastIP(192,168,4,255);
+
 void setup() {
   Serial.begin(115200);
   mySerial.begin(416666, SERIAL_8N1, 16, 17, true);  // UART2 (RX=16, TX=17)
+
+  WiFi.softAP(ssid, password);
+
+  Serial.println("Access Point started!");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.softAPIP()); // default is usually 192.168.4.1
+
+  udp.begin(udpPort);
 }
 
 void loop() {
@@ -34,6 +52,12 @@ void loop() {
       }
     }
   }
+}
+
+void sendUDP(uint8_t *data, size_t len) {
+  udp.beginPacket(broadcastIP, udpPort);
+  udp.write(data, len);
+  udp.endPacket();
 }
 
 void printHex(uint8_t hex_value){
