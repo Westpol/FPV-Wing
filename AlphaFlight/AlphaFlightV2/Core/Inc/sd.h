d94572fa801457d5f85b077cbf23f0a909aec38f
Benno <81779508+Westpol@users.noreply.github.com>
2025-09-12 15:42:33 +0200

    Created sd.c interface

---
+++ AlphaFlight/AlphaFlightV2/Core/Inc/sd.h
@@ -0,0 +1,13 @@
+/*
+ * sd.h
+ *
+ *  Created on: Sep 12, 2025
+ *      Author: benno
+ */
+
+#ifndef INC_SD_H_
+#define INC_SD_H_
+
+
+
+#endif /* INC_SD_H_ */
---
+++ AlphaFlight/AlphaFlightV2/Core/Src/sd.c
@@ -0,0 +1,8 @@
+/*
+ * sd.c
+ *
+ *  Created on: Sep 12, 2025
+ *      Author: benno
+ */
+
+#include "sd.h"
