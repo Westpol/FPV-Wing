bc486de73cf7c8fb02a1b74f1a26aa636903e27e
Benno <81779508+Westpol@users.noreply.github.com>
2025-10-06 10:01:13 +0200

    Added Scheduler overrun detection, added global max loop time

--- AlphaFlight/AlphaFlightV2/Core/Src/usage_stats.c
+++ AlphaFlight/AlphaFlightV2/Core/Src/usage_stats.c
@@ -14,8 +14,15 @@ static uint32_t max_loop_time_global = 0;
 static uint32_t max_loop_time = 0;
 static uint32_t max_loop_time_1s = 0;
 static uint64_t second_reset_counter = 1000000;
+static uint64_t scheduler_not_finished = 0;
+static uint64_t scheduler_finished = 0;
 
 void USAGE_STAT_START_OF_SCHEDULER_CALL(void){
+	if(start_time_us != 0){
+		scheduler_not_finished++;
+		return;
+	}
+	scheduler_finished++;
 	start_time_us = MICROS64();
 }
 
@@ -28,11 +35,15 @@ void USAGE_STAT_END_OF_SCHEDULER_CALL(void){
 	if(delta_t > max_loop_time){
 		max_loop_time = delta_t;
 	}
-	if(MICROS64() > second_reset_counter){
+	if(delta_t > max_loop_time_global){
+		max_loop_time_global = delta_t;
+	}
+	if(now > second_reset_counter){
 		second_reset_counter += 1000000;
 		max_loop_time_1s = max_loop_time;
 		max_loop_time = 0;
 	}
+	start_time_us = 0;
 }
 
 float USAGE_STAT_GET_AVRG_1S(void){
@@ -41,4 +52,12 @@ float USAGE_STAT_GET_AVRG_1S(void){
 
 uint32_t USAGE_STAT_GET_MAX_LOOP_TIME_1S(void){
 	return max_loop_time_1s;
+}
+
+float USAGE_STAT_GET_SCHEDULER_NOT_FINISHED_PERCENT(void){
+	double total = (double)scheduler_finished + (double)scheduler_not_finished;
+	if(total == 0){
+		return 0;
+	}
+	return ((double)scheduler_not_finished / total) * 100.0;
 }
