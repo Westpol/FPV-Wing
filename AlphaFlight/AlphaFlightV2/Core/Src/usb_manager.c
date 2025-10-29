2570dd332fc1eafef01af94568f29808acb87a45
Benno <81779508+Westpol@users.noreply.github.com>
2025-10-29 14:13:07 +0100

    Add Configurator basics

--- AlphaFlight/AlphaFlightV2/Core/Src/usb_manager.c
+++ AlphaFlight/AlphaFlightV2/Core/Src/usb_manager.c
@@ -15,12 +15,14 @@
 #include <string.h>
 
 uint8_t cdc_buffer[APP_RX_DATA_SIZE] = {0};
+static uint8_t clear_buffer[64];
 
 extern USBD_HandleTypeDef hUsbDeviceFS;
 TIM_HandleTypeDef* MICROS64_HTIM = NULL;
 
 bool enter_usb_mode_next_loop = false;
 bool usb_config_mode_enabled = false;
+bool exit_command_line_config = false;
 
 void USB_INIT(){
 	MICROS64_HTIM = TIME_UTILS_GET_TIMER();
@@ -28,6 +30,9 @@ void USB_INIT(){
 		ERROR_HANDLER_BLINKS(1);
 	}
 	USB_CHECK_FOR_CONNECTION();
+	for(int i = 0; i < 64;i++){
+		clear_buffer[i] = (i % 2 == 0)? 13 : 10;
+	}
 }
 
 
@@ -43,7 +48,8 @@ void USB_CHECK_FOR_CONNECTION(){
 				__HAL_TIM_DISABLE(MICROS64_HTIM);
 
 				while(1){
-					if(hUsbDeviceFS.dev_state != USBD_STATE_CONFIGURED){
+					if(hUsbDeviceFS.dev_state != USBD_STATE_CONFIGURED || exit_command_line_config){
+						exit_command_line_config = false;
 						FLIGHT_STATE_USB_DISABLE_OVERRIDE(FLIGHT_STATE_USB_OVERRIDE_KEY);
 						break;
 					}
@@ -66,8 +72,19 @@ void USB_DECODE_COMMAND(uint32_t length){	// set commands / variables here
 	uint32_t expected_len = sizeof(expected) - 1;
 	if(length >= expected_len){
 		if(memcmp(cdc_buffer, expected, expected_len) == 0){
-			USBD_CDC_SetTxBuffer(&hUsbDeviceFS, cdc_buffer, length);
+			USBD_CDC_SetTxBuffer(&hUsbDeviceFS, clear_buffer, 64);
 			USBD_CDC_TransmitPacket(&hUsbDeviceFS);
+		}
+	}
+
+	const char expected_exit[] = "exit";
+	uint32_t expected_len_exit = sizeof(expected_exit) - 1;
+	if(length >= expected_len_exit){
+		if(memcmp(cdc_buffer, expected_exit, expected_len_exit) == 0){
+			USB_PRINTLN_BLOCKING("exiting command line configuration...");
+			usb_config_mode_enabled = false;
+			exit_command_line_config = true;
+			return;
 		}
 	}
 }
