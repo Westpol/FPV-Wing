bcfe4be44a4ea79a12b24b85cd6bde6613bac23d
Benno <81779508+Westpol@users.noreply.github.com>
2025-10-29 22:49:19 +0100

    Created cinfig_data source files

---
+++ AlphaFlight/AlphaFlightV2/Core/Inc/config_data.h
@@ -0,0 +1,13 @@
+/*
+ * config_data.h
+ *
+ *  Created on: Oct 29, 2025
+ *      Author: benno
+ */
+
+#ifndef INC_CONFIG_DATA_H_
+#define INC_CONFIG_DATA_H_
+
+
+
+#endif /* INC_CONFIG_DATA_H_ */
---
+++ AlphaFlight/AlphaFlightV2/Core/Src/config_data.c
@@ -0,0 +1,8 @@
+/*
+ * config_data.c
+ *
+ *  Created on: Oct 29, 2025
+ *      Author: benno
+ */
+
+#include "config_data.h"
